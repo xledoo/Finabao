7a762a88d7419eea05c2674ce418488f4faab226
alipiapia <124910168@qq.com>
2014-11-20 20:27:49 +0800

    Add source/finabao/class/register_ctl.php

--- source/finabao/controller/controller_login.php
+++ source/finabao/controller/controller_login.php
@@ -1,4 +1,14 @@
 <?php
 
-include template('member/login');
+
+	require_once libfile('function/member');
+	require_once './source/finabao/class/logging_ctl.php';
+
+	$ctl_obj = new logging_ctl();
+	$ctl_obj->setting = $_G['setting'];
+	$method = 'on_'.$_GET['action'];
+	$ctl_obj->template = 'member/login';
+	$ctl_obj->on_login();
+
+
 ?>
--- source/finabao/controller/controller_register.php
+++ source/finabao/controller/controller_register.php
@@ -1,5 +1,12 @@
 <?php
 
-// debug($_G['setting']['bbclosed']);
-include template('member/register');
+    require_once libfile('function/member');
+    require_once './source/finabao/class/register_ctl.php';
+
+    $ctl_obj            =   new register_ctl();
+    $ctl_obj->setting   =   $_G['setting'];
+    $ctl_obj->template  =   'member/register';
+
+    $ctl_obj->on_register();
+
 ?>
